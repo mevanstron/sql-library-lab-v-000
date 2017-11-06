0ef61ba2de9833c6211faab899a2094697ead839
Michael Evans <michael@michaelgregoryevans.com>
2017-11-06 20:17:33 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters
-   WHERE LENGTH(motto) = SELECT MAX (LENGTH(motto)) FROM characters"
+   WHERE LENGTH(motto) = (SELECT MAX (CHAR_LENGTH(motto)) FROM characters)"
 end
 
 
