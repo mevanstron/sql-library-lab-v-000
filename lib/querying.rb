3681ae0bb627c5e6a4b547ed1c57a4b6dab35de8
Michael Evans <michael@michaelgregoryevans.com>
2017-11-06 20:20:55 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT names, species, species AS species_count FROM characters
+   GROUP BY species_count ORDER BY DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
