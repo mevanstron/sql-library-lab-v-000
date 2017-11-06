2900e147d82aab7c313b19002b6974be5911e6c8
Michael Evans <michael@michaelgregoryevans.com>
2017-11-06 21:09:37 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,7 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) FROM characters
-   WHERE COUNT(species) = (SELECT MAX (COUNT(species)) FROM characters)"
+   HAVING COUNT(species) = (SELECT MAX (COUNT(species)) FROM characters)"
 end
 
 def select_name_and_series_subgenres_of_authors
