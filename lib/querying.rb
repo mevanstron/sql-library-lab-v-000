10bb367a3e5cad9ec168c9c9da0376879f5e6f14
Michael Evans <michael@michaelgregoryevans.com>
2017-11-06 22:01:07 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,13 +29,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species FROM characters
+  "SELECT series.title FROM characters
    INNER JOIN series
    ON characters.series_id = series.id
    WHERE characters.species = 'human'
    GROUP BY series.title
    ORDER BY COUNT(series.title) DESC LIMIT 1
-   
+
      "
 end
 
